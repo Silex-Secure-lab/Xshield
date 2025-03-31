dbc38922d1e85bb4ed6ebc30ee85bb199d7d79ce
Caleb <calebshokunbi619@gmail.com>
2025-03-30 22:59:01 -1200

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 // import About from "./pages/About";
-import NotFound from "./pages/NotFound";
+// import NotFound from "./pages/NotFound";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Services from "./pages/Services";
@@ -14,8 +14,6 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/services" element={<Services />} />
-        {/* <Route path="/about" element={<About />} /> */}
-        <Route path="*" element={<NotFound />} /> {/* 404 Page */}
       </Routes>
     </Router>
   );
